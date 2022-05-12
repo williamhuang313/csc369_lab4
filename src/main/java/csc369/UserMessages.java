16743b4833550cc85d75043217973b039f127e8f
williamhuang313 <williamhuang313@gmail.com>
2022-05-12 16:24:09 -0700

    Added R1_Join

--- src/main/java/csc369/UserMessages.java
+++ src/main/java/csc369/UserMessages.java
@@ -47,12 +47,16 @@ public class UserMessages {
 
 	@Override
 	    public void reduce(Text key, Iterable<Text> values, Context context)  throws IOException, InterruptedException {
-	    ArrayList<String> name = new ArrayList();
-	    ArrayList<String> messages = new ArrayList();
+	    int counter = 0;
+		Text[] values_list;
 
+		
 	    for (Text val : values) {
-		context.write(key, val);
+		values_list[counter] = val;
+		counter += 1;
 	    }
+
+		context.write(values_list[0], values_list[1]);
 	}
     } 
 
